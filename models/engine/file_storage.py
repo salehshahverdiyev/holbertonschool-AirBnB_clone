1f6ad3b8e976a83d4f94d4db9c9d4e174027d8ed
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-02-27 08:42:52 +0400

    Created engine file_storege.py

---
+++ models/engine/file_storage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+import json
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+    
+    def new(self, obj):
+        class_name = obj.__class__.__name__
+        FileStorage.__objects[f"{class_name}.{obj.id}"] = obj
+
+    def save(self):
+        with open(FileStorage.__file_path, "w") as f:
+            f.write(json.dumps(FileStorage.__objects))
