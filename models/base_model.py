8d3ceecf8dc9d961825040162ec1bceb3aecbb34
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-02-27 08:55:13 +0400

    Add storage methods to base model

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 
 
 import uuid
+from models import storage
 from datetime import datetime
 
 class BaseModel:
@@ -18,6 +19,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         class_name = self.__class__.__name__
@@ -29,6 +31,7 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
